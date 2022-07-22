1ba083b84014b14ef94da325202353416ceb5b18
diegoalpin <diego_alpin@live.com>
2022-07-22 12:45:22 +0800

    users controller added for get AllExchangeRates

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,1 +1,20 @@
 const user_model = require("../models/users");
+import express  from "express";
+
+const router = express.Router();
+
+var currencies=[
+    {
+    "id": 1,
+    "base_currency": "SGD",
+    "exchange_currency": "CAD",
+    "rate": 0.9255
+    },
+];
+
+router.get('/', getAllExhangeRates = (req,res)=>{
+    console.log(currencies);
+    res.send(currencies);
+});
+
+export default router;
