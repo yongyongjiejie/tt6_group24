b07921474ae879479ff4bc01b4f4ecd2e80ac639
Zai Hao <zh>
2022-07-22 16:56:53 +0800

    fix background image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
 import companyLogo from './images/dbs_logo.png';
+import loginbg from './images/login_bg.jpg';
 import { Link } from "react-router-dom";
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-
 function App() {
   const [apiResponse, setApiResponse] = React.useState("");
 
@@ -25,9 +25,16 @@ function App() {
 
   return (
     <div className="Container">
-      <div className="main">
+      <div className="main"  style={{ 
+      "backgroundImage": `url(${loginbg})`,
+      "background-position": "center",
+      "background-repeat": "no-repeat",
+      "background-size": "cover"
+    }}>
+          
         <img id="companyLogo" src={companyLogo} alt="BigCo Inc. logo"/>
-        <h2 className="main-header">DBS SEED Team 24 Login</h2>
+        <h2 className="main-header" style={{ 
+      "color": "black"}}>DBS SEED Team 24 Login</h2>
         <nav> 
           <div className="login-box">
             <Login/>
