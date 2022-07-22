1e59ee081a099cfee42d9b892a722cd31b6b753c
jj-dk <1700010@sit.singaporetech.edu.sg>
2022-07-22 14:13:43 +0800

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Login from './components/login';
-import Main from './components/main'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="Login" element={<Login />} />
-        <Route path="Main" element={<Main />} />
-      </Routes>
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -30,6 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login";
 import Wallet from "./components/wallet";
 import Transaction from "./components/transaction";
+import Main from "./components/main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -39,8 +18,8 @@ root.render(
       <Route path="Login" element={<Login />} />
       <Route path="wallet" element={<Wallet />} />
       <Route path="transaction" element={<Transaction />} />
+      <Route path="Main" element={<Main />} />
     </Routes>
->>>>>>> 41104a2873fe03e93cb89a79f7cf3b34e839d191
   </BrowserRouter>
 );
 
