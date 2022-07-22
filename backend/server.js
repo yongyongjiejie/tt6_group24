24e1c95069979c8a1c64b77f9709592f7a7a3d99
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2022-07-22 12:51:54 +0800

    successful connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,23 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const PORT = process.env.PORT;
 const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+// Set up connection to mongo db
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("successfully connected");
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
+
+// open the connection to mongo
+mongoose.connection.on("open", () => {});
