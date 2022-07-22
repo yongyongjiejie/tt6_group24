5140a53e4ccdf0fff5328195b2f228d630af1f27
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2022-07-22 12:37:57 +0800

    added mongoURI string

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+mongoose.set("useFindAndModify", false);
