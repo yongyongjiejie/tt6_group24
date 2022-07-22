ad6a1be2a9606993480d458d667adf3efe73a1c8
jj-dk <1700010@sit.singaporetech.edu.sg>
2022-07-22 15:02:22 +0800

    Updated wallet table

--- frontend/src/components/main.js
+++ frontend/src/components/main.js
@@ -1,48 +1,47 @@
-import * as React from 'react'
-import '../css/main.css'
-import { fetchExchangeRates } from '../ExRates/ExRates';
-import Currency from './Currency.js'
+import * as React from "react";
+import "../css/main.css";
+//import { fetchExchangeRates } from "../ExRates/ExRates";
+import Currency from "./currency.js";
 export default function Main() {
+  const [rates, setRates] = React.useState(null);
+  const [currencyBase, setcurrencyBase] = React.useState("");
 
-    const [rates,setRates] = React.useState(null)
-    const [currencyBase, setcurrencyBase] = React.useState('')
+  React.useEffect(() => {
+    let componentMounted = true;
+    // const getExchangeRates = () => {
+    //   //backend
+    //   fetchExchangeRates()
+    //     .then((data) => {
+    //       console.log("Exchange Rate:", data);
+    //       if (componentMounted) {
+    //         setRates(data.rates);
+    //         setcurrencyBase(data.base);
+    //       }
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // };
 
-    React.useEffect(()=>{
-        let componentMounted=true;
-        const getExchangeRates = () =>{
-            //backend
-            fetchExchangeRates()
-            .then((data) => {
-                console.log('Exchange Rate:', data)
-                if(componentMounted){
-                    setRates(data.rates);
-                    setcurrencyBase(data.base)
-                }
-            })
-            .catch((err)=>{
-                console.log(err)
-            });
-        };
-        
-        getExchangeRates();
+    // getExchangeRates();
 
-        return()=>{
-            componentMounted = false
-        };
-    },[]);
+    return () => {
+      componentMounted = false;
+    };
+  }, []);
 
   return (
-    <div className='main-exrate'>
-      <h1>
-        Exchange Rates
-      </h1>
-      {rates ? Object.keys(rates).map((key)=>(
-      <Currency exchangeRateSymbol={key}
-                exchangeRate={rates[key]}
-                currencyBase={currencyBase}
-        />
-        )): []}
+    <div className="main-exrate">
+      <h1>Exchange Rates</h1>
+      {rates
+        ? Object.keys(rates).map((key) => (
+            <Currency
+              exchangeRateSymbol={key}
+              exchangeRate={rates[key]}
+              currencyBase={currencyBase}
+            />
+          ))
+        : []}
     </div>
   );
 }
-  
